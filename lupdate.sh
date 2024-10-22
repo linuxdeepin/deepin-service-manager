afa2d0464382a8f50780e4437990a27ef27c40a6
fuleyi <fuleyi@uniontech.com>
2024-10-22 15:24:28 +0800

    chore: 去掉插件包，插件整合在deepin-service-plugins项目中
    去掉插件包，插件整合在deepin-service-plugins项目中

--- lupdate.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
-#
-# SPDX-License-Identifier: MIT
-
-cd ./src/plugin-sdbus/plugin-oom
-sh lupdate.sh
-cd ../../../
-
-tx push -s --branch m20
