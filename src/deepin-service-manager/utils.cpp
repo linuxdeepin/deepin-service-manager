c9a9131064d645ec42011e4a9820e6e2f5f25624
Wang Zichong <wangzichong@deepin.org>
2025-10-31 15:48:49 +0800

    feat: check Qt version w/o actually loading so file
    历史代码中,会尝试使用 dlopen() 装载 Qt 插件文件,调用 qVersion 获取
    插件版本信息,并 dlclose() 卸载插件文件. 这在 Qt 6.8.2+ 会导致崩溃
    在 QCoreApplicationPrivate::sendThroughApplicationEvent, 由 qtbase
    的 2b3e56e2b4198a940bb811964d0342868c391858 所致.
    
    此处的目的仅仅是检查插件版本,而 dlopen() 本身即可能会执行一些代码,
    导致污染当前线程,故转为使用解析 so 文件的 ELF 头的 .qtversion section
    来解析 Qt 版本号,并优先使用此版本号进行判断.
    
    此修改还增加了一个命令行选项用来验证 so 文件的 Qt 版本解析,以供临时
    调试类场景使用.
    
    Log:

--- src/deepin-service-manager/utils.cpp
+++ src/deepin-service-manager/utils.cpp
@@ -4,10 +4,14 @@
 
 #include  "utils.h"
 
-#include <QPluginLoader>
+#include <QtEndian>
+#include <QFile>
 #include <QLibrary>
-#include <QVersionNumber>
 #include <QLoggingCategory>
+
+#ifdef ELF_QT_VER_CHECK
+#include <elf.h>
+#endif // ELF_QT_VER_CHECK
 
 #include <cstring>
 #include <sys/prctl.h>
@@ -38,6 +42,24 @@ void setProcessName(int argc, char **argv, const char *title)
 
 bool checkLibraryQtVersion(const QString &soPath)
 {
+    QVersionNumber libraryQtVersion = qtVersionFromSo(soPath);
+    QVersionNumber expectedQtVersion = QVersionNumber::fromString(QString::fromUtf8(&USE_QT_VERSION_MAJOR));
+
+    auto checkQtVersion = [](const QVersionNumber &libraryQtVersion, const QVersionNumber &expectedQtVersion) -> bool {
+        if (libraryQtVersion.majorVersion() == expectedQtVersion.majorVersion()) {
+            return true;
+        }
+
+        qCWarning(dsm_utils) << "This library links against Qt" << libraryQtVersion.toString()
+                             << "but expected Qt" << expectedQtVersion.majorVersion();
+        return false;
+    };
+
+    // 优先从 so 文件的 ELF 头解析 Qt 版本信息，避免装载（dlopen）动态库
+    if (!libraryQtVersion.isNull()) {
+        return checkQtVersion(libraryQtVersion, expectedQtVersion);
+    }
+
     QLibrary library(soPath);
     if (!library.load()) {
         qCWarning(dsm_utils) << "Error opening library:" << soPath << library.errorString();
@@ -56,15 +78,84 @@ bool checkLibraryQtVersion(const QString &soPath)
     const char *qtVersion = qVersionFunc();
     qCInfo(dsm_utils) << "Library Qt version:" << qtVersion;
 
-    QVersionNumber libraryQtVersion = QVersionNumber::fromString(QString::fromUtf8(qtVersion));
-    QVersionNumber expectedQtVersion = QVersionNumber::fromString(QString::fromUtf8(&USE_QT_VERSION_MAJOR));
+    libraryQtVersion = QVersionNumber::fromString(QString::fromUtf8(qtVersion));
 
-    // 检查主版本号
-    if (libraryQtVersion.majorVersion() == expectedQtVersion.majorVersion()) {
-        return true;
+    return checkQtVersion(libraryQtVersion, expectedQtVersion);
+}
+
+/*
+ * Parse version from so file by reading `.qtversion` section in ELF header.
+ *
+ * Return a null QVersionNumber if failed to parse Qt version from ELF header.
+ *
+ * Following paragraph is quoted from qtbase/src/corelib/global/qversiontagging.h
+ *
+ * It's also possible to inspect which version is required by decoding the
+ * .qtversion section. The second pointer-sized variable is the required
+ * version, for example, for Qt 6.4.1:
+ *
+ *      Hex dump of section [18] '.qtversion', 16 bytes at offset 0x1ee48:
+ *        0x00000000 b0ffffff ffffffff 01040600 00000000 ................
+ *                                     ^^^^^^^^ ^^^^^^^^
+ */
+QVersionNumber qtVersionFromSo(const QString &path)
+{
+#ifdef ELF_QT_VER_CHECK
+    QFile f(path);
+    if (!f.open(QIODevice::ReadOnly)) {
+        qWarning() << "Cannot open file:" << path;
+        return {};
     }
 
-    qCWarning(dsm_utils) << "This library links against Qt" << libraryQtVersion.toString()
-                         << "but expected Qt" << expectedQtVersion.majorVersion();
-    return false;
+    Elf64_Ehdr ehdr;
+    if (f.read(reinterpret_cast<char*>(&ehdr), sizeof(ehdr)) != sizeof(ehdr))
+        return {};
+    if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0)
+        return {};
+
+    const bool is64 = (ehdr.e_ident[EI_CLASS] == ELFCLASS64);
+    const bool isLE = (ehdr.e_ident[EI_DATA] == ELFDATA2LSB);
+    const size_t ptrSize = is64 ? 8 : 4;
+
+    // 读取节头表
+    f.seek(ehdr.e_shoff);
+    QVector<Elf64_Shdr> sections(ehdr.e_shnum);
+    if (f.read(reinterpret_cast<char*>(sections.data()), sizeof(Elf64_Shdr) * ehdr.e_shnum)
+        != qint64(sizeof(Elf64_Shdr) * ehdr.e_shnum))
+        return {};
+
+    // 读取 section 名称表
+    const Elf64_Shdr &shstr = sections[ehdr.e_shstrndx];
+    QByteArray shstrtab(shstr.sh_size, Qt::Uninitialized);
+    f.seek(shstr.sh_offset);
+    f.read(shstrtab.data(), shstr.sh_size);
+
+    // 查找 ".qtversion" 节
+    for (const Elf64_Shdr &sh : sections) {
+        const char *secName = shstrtab.constData() + sh.sh_name;
+        if (qstrcmp(secName, ".qtversion") != 0)
+            continue;
+
+        QByteArray sec(sh.sh_size, Qt::Uninitialized);
+        f.seek(sh.sh_offset);
+        f.read(sec.data(), sh.sh_size);
+
+        if (size_t(sec.size()) < ptrSize * 2)
+            return {};
+
+        // 版本号在第二个指针的位置
+        quint32 verRaw = 0;
+        memcpy(&verRaw, sec.constData() + ptrSize, 4); // 只取低4字节
+        quint32 ver = isLE ? qFromLittleEndian(verRaw) : qFromBigEndian(verRaw);
+
+        int major = (ver >> 16) & 0xFF;
+        int minor = (ver >> 8)  & 0xFF;
+        int patch = ver & 0xFF;
+
+        return QVersionNumber(major, minor, patch);
+    }
+#else // ELF_QT_VER_CHECK
+    Q_UNUSED(path);
+#endif // ELF_QT_VER_CHECK
+    return {};
 }
